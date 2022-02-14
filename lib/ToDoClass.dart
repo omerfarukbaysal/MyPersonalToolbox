9bc530d97720f0cc37b7c40a4685bc837474bb86
Omer Faruk <mrfarukbaysal@gmail.com>
2022-02-14 03:35:03 +0300

    initial commit

--- lib/ToDoClass.dart
+++ lib/ToDoClass.dart
@@ -2,12 +2,9 @@ class ToDo {
   int _id;
   String _title;
   bool _isChecked;
-  //This is Optioanl Position Paremeter if {} This is Optional Named Parameter
   ToDo(this._title, this._isChecked);
-  //This is during editing(Called with Id)
   ToDo.withId(this._id, this._title, this._isChecked);
 
-//All the Getters(Controls the Data Asked By another method from this Class)
   int get id {
     return _id;
   }
@@ -15,7 +12,6 @@ class ToDo {
   String get title => _title;
   bool get isChecked => _isChecked;
 
-//These are all the Setters
   set title(String newTitle) {
     if (newTitle.length <= 255) {
       this._title = newTitle;
@@ -26,12 +22,8 @@ class ToDo {
     this._isChecked = newChecked;
   }
 
-//Used to Save and Retrive from the Database
-//Converting the ToDo Object into Map Object
   Map<String, dynamic> toMap() {
-    //This is an Map Object
     var map = Map<String, dynamic>();
-    //This means Already Created in the Database
     if (id != null) {
       map['id'] = _id;
     }
--- lib/database_helper.dart
+++ lib/database_helper.dart
@@ -5,8 +5,8 @@ import 'dart:io';
 import 'package:path_provider/path_provider.dart';
 
 class DatabaseHelper {
-  static DatabaseHelper _databaseHelper; //Singleton
-  static Database _database; //singleton
+  static DatabaseHelper _databaseHelper;
+  static Database _database;
 
   String todoTable = 'todo_table';
   String colID = 'id';
@@ -22,7 +22,6 @@ class DatabaseHelper {
     return _databaseHelper;
   }
 
-//custom getter
   Future<Database> get database async {
     if (_database == null) {
       _database = await initalizeDatabase();
@@ -46,8 +45,6 @@ class DatabaseHelper {
 
   Future<List<Map<String, dynamic>>> getNoteMapList() async {
     Database db = await this.database;
-    //optional
-    // var result = await db.rawQuery('SELECT * from $todoTable order by $colPriority ASC');
     var result = await db.query(todoTable);
     return result;
   }
@@ -92,91 +89,3 @@ class DatabaseHelper {
     return noteList;
   }
 }
-
-/* import 'dart:io';
-
-import 'package:path/path.dart';
-import 'package:sqflite/sqflite.dart';
-import 'package:path_provider/path_provider.dart';
-
-class DatabaseHelper {
-  static final _databaseName = "MyDatabase.db";
-  static final _databaseVersion = 1;
-
-  static final table = 'todolist';
-
-  static final columnId = '_id';
-  static final columnTitle = "title";
-
-  // make this a singleton class
-  DatabaseHelper._privateConstructor();
-  static final DatabaseHelper instance = DatabaseHelper._privateConstructor();
-
-  // only have a single app-wide reference to the database
-  static Database _database;
-  Future<Database> get database async {
-    if (_database != null) return _database;
-    // lazily instantiate the db the first time it is accessed
-    _database = await _initDatabase();
-    return _database;
-  }
-
-  // this opens the database (and creates it if it doesn't exist)
-  _initDatabase() async {
-    Directory documentsDirectory = await getApplicationDocumentsDirectory();
-    String path = join(documentsDirectory.path, _databaseName);
-    return await openDatabase(path,
-        version: _databaseVersion, onCreate: _onCreate);
-  }
-
-  // SQL code to create the database table
-  Future _onCreate(Database db, int version) async {
-    await db.execute('''
-          CREATE TABLE $table (
-            $columnId INTEGER PRIMARY KEY,
-            $columnTitle TEXT NOT NULL
-          );
-          ''');
-  }
-
-  // Helper methods
-
-  // Inserts a row in the database where each key in the Map is a column name
-  // and the value is the column value. The return value is the id of the
-  // inserted row.
-  Future<int> insert(Map<String, dynamic> row) async {
-    Database db = await instance.database;
-    return await db.insert(table, row);
-  }
-
-  // All of the rows are returned as a list of maps, where each map is
-  // a key-value list of columns.
-  Future<List<Map<String, dynamic>>> queryAllRows() async {
-    Database db = await instance.database;
-    return await db.query(table);
-  }
-
-  // All of the methods (insert, query, update, delete) can also be done using
-  // raw SQL commands. This method uses a raw query to give the row count.
-  Future<int> queryRowCount() async {
-    Database db = await instance.database;
-    return Sqflite.firstIntValue(
-        await db.rawQuery('SELECT COUNT(*) FROM $table'));
-  }
-
-  // We are assuming here that the id column in the map is set. The other
-  // column values will be used to update the row.
-  Future<int> update(Map<String, dynamic> row) async {
-    Database db = await instance.database;
-    int id = row[columnId];
-    return await db.update(table, row, where: '$columnId = ?', whereArgs: [id]);
-  }
-
-  // Deletes the row specified by the id. The number of affected rows is
-  // returned. This should be 1 as long as the row exists.
-  Future<int> delete(int id) async {
-    Database db = await instance.database;
-    return await db.delete(table, where: '$columnId = ?', whereArgs: [id]);
-  }
-}
- */
